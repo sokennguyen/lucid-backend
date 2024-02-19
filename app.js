18e8e6af14bd4d32c1ca1cfca7ae004ec747f1fd
soken <nsongkien@gmail.com>
2024-02-19 15:05:33 +0200

    cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express()
 const dreamRouter = require('./controllers/dream')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const cors = require('cors')
 
 mongoose.set('strictQuery',false)
 mongoose.connect(config.MONGODB_URI).then(
     console.log('Connected to MongoDB')
 )
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/dream',dreamRouter)
